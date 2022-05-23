4763c92d27fd1418826ad7a83d4bb8e8f83bab04
Jay Mendes <jean-mendes@live.fr>
2022-05-23 18:11:16 +0200

    Creation bo basique classes + tout guetters setters

---
+++ ProjetEncheres/src/main/java/bo/Articles.java
@@ -0,0 +1,75 @@
+package bo;
+
+import java.time.LocalDate;
+
+public class Articles {
+	
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEnchere;
+	private LocalDate dateFinEnchere;
+	private int prixInitial;
+	private int prixVente;
+	private int noUtilisateur;
+	private int noCategorie;
+	
+	
+	public int getNoArticle() {
+		return noArticle;
+	}
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	public String getNomArticle() {
+		return nomArticle;
+	}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getDateDebutEnchere() {
+		return dateDebutEnchere;
+	}
+	public void setDateDebutEnchere(LocalDate dateDebutEnchere) {
+		this.dateDebutEnchere = dateDebutEnchere;
+	}
+	public LocalDate getDateFinEnchere() {
+		return dateFinEnchere;
+	}
+	public void setDateFinEnchere(LocalDate dateFinEnchere) {
+		this.dateFinEnchere = dateFinEnchere;
+	}
+	public int getPrixInitial() {
+		return prixInitial;
+	}
+	public void setPrixInitial(int prixInitial) {
+		this.prixInitial = prixInitial;
+	}
+	public int getPrixVente() {
+		return prixVente;
+	}
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+	
+	
+	
+}
