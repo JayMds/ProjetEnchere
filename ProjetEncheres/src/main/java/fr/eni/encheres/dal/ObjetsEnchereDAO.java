dfa406203225a77e7767de408e7435f1a0aa3223
Jay Mendes <jean-mendes@live.fr>
2022-05-24 14:45:34 +0200

    maj DAO

--- ProjetEncheres/src/main/java/fr/eni/encheres/dal/ObjetsEnchereDAO.java
+++ ProjetEncheres/src/main/java/fr/eni/encheres/dal/ObjetsEnchereDAO.java
@@ -1,12 +1,15 @@
 package fr.eni.encheres.dal;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ObjetsEnchereDAO<T> {
 	
 	public void insert();
-	public T selectById();
+	public T selectById(int id);
 	public List<T> selectAll();
-	public void delete();
+	public void delete(int id);
+	public List<T> selectDateEnCours(LocalDate date);
 
+	
 }
