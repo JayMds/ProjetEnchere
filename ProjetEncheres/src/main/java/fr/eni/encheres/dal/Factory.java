d0d2adf8dcc6a4c6c89c4ec9ce8181c7b1aaa51a
alexgitENI <alexgalaxy67@gmail.com>
2022-05-24 18:19:20 +0200

    Factory

---
+++ ProjetEncheres/src/main/java/fr/eni/encheres/dal/Factory.java
@@ -0,0 +1,16 @@
+package fr.eni.encheres.dal;
+
+import fr.eni.encheres.bo.Utilisateur;
+
+public class Factory {
+	
+	
+	public static class factory{
+		public static ObjetsEnchereDAO<Utilisateur> getUtilisateurDAO() {
+			return new UtilisateurDAOImpl();
+		}
+
+	}
+
+
+}
