3f1f678ebb3fc5599763b72ab1ef580721b25df6
Jay Mendes <jean-mendes@live.fr>
2022-05-24 15:13:32 +0200

    connProvid en piblic + selectAll Article :

--- ProjetEncheres/src/main/java/fr/eni/encheres/dal/jdbc/ArticleDAOJDBCImpl.java
+++ ProjetEncheres/src/main/java/fr/eni/encheres/dal/jdbc/ArticleDAOJDBCImpl.java
@@ -1,12 +1,20 @@
 package fr.eni.encheres.dal.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.encheres.bo.Article;
 import fr.eni.encheres.dal.ObjetsEnchereDAO;
+import fr.eni.encheres.dal.ConnectionProvider;
+
 
 public class ArticleDAOJDBCImpl implements ObjetsEnchereDAO<Article>{
+	
+	private final String selectAllArticles = "select * from Articles; ";
 
 	@Override
 	public void insert() {
@@ -22,8 +30,27 @@ public class ArticleDAOJDBCImpl implements ObjetsEnchereDAO<Article>{
 
 	@Override
 	public List<Article> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+	//Création Liste
+		List<Article> articles = new ArrayList<>();
+		Article a = null;
+		
+			try(Connection cnx = ConnectionProvider.getConnection();)
+			{
+				Statement stmt = cnx.createStatement();
+				ResultSet rs = stmt.executeQuery(selectAllArticles);
+				while (rs.next()) {
+					a = new Article();
+					articles.add(a);
+				}
+			
+				stmt.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		
+		return articles;
 	}
 
 	@Override
