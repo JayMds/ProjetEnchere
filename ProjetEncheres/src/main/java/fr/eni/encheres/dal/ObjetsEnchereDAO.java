52d5fed7f23262f8a97e51d3a0bf443efdf1723f
Jay Mendes <jean-mendes@live.fr>
2022-05-25 10:07:54 +0200

    maj objetsdao

--- ProjetEncheres/src/main/java/fr/eni/encheres/dal/ObjetsEnchereDAO.java
+++ ProjetEncheres/src/main/java/fr/eni/encheres/dal/ObjetsEnchereDAO.java
@@ -1,6 +1,5 @@
 package fr.eni.encheres.dal;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ObjetsEnchereDAO<T> {
@@ -10,7 +9,6 @@ public interface ObjetsEnchereDAO<T> {
 	public T selectById(int id) throws DALException;
 	public List<T> selectAll() throws DALException;
 	public void delete(int id) throws DALException;
-	public List<T> selectDateEnCours(LocalDate date) throws DALException;
 
 	
 }
