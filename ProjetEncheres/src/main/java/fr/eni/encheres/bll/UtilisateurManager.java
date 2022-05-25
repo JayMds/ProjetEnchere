33e59c24080406d052811430c91396bec52d2612
alexgitENI <alexgalaxy67@gmail.com>
2022-05-25 16:30:31 +0200

    a

--- ProjetEncheres/src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
+++ ProjetEncheres/src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
@@ -8,20 +8,15 @@ import fr.eni.encheres.dal.DALException;
 import fr.eni.encheres.dal.DAOFactory;
 import fr.eni.encheres.dal.ObjetsEnchereDAO;
 
-public class UtilisateurManager {
+public class UtilisateurManager extends VerificationUtilisateurManager {
 
-	private static int TAILLE_MINI = 3;
-	private static int TAILLE_MAXI = 30;
-
-	private static String REGEX_CODE_POSTAL = "/^(([0-8][0-9])|(9[0-5]))[0-9]{3}$/";
-	private static String REGEX_TELEPHONE = "^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$";
-	private static String REGEX_VILLE = "[^0-9]";
-	private static String REGEX_MOT_DE_PASSE = "^(?=.*[0-9])(?=.*[az])(?=.*[AZ])(?=.*[@#$%^&-+=() ])(?=\\\\S+$).{8, 20}$";
 
 	private ObjetsEnchereDAO<Utilisateur> utilisateurDAO;
 
+
 	public UtilisateurManager() {
-		this.utilisateurDAO = DAOFactory.getUtilisateurDAO(); 
+		this.utilisateurDAO = DAOFactory.getUtilisateurDAO();
+
 	}
 
 	public Utilisateur ajouterutilisateur(String pseudo, String nom, String prenom, String email, String telephone,
@@ -65,114 +60,7 @@ public class UtilisateurManager {
 		}
 		return utilisateur;
 	}
-
-	private void validerPseudo(String pseudo, BusinessException businessException) {
-
-		if (pseudo.isBlank() ^ pseudo.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-		}
-
-		if (pseudo.length() < TAILLE_MINI ^ pseudo.length() > TAILLE_MAXI) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_ERREUR_FORMAT);
-		}
-
-	}
-
-	private void validerNom(String nom, BusinessException businessException) {
-
-		if (nom.isBlank() ^ nom.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-		}
-
-		if (nom.length() < TAILLE_MINI ^ nom.length() > TAILLE_MAXI) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_ERREUR_FORMAT);
-		}
-
-	}
-
-	private void validerPrenom(String prenom, BusinessException businessException) {
-
-		if (prenom.isBlank() ^ prenom.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-		}
-
-		if (prenom.length() < TAILLE_MINI ^ prenom.length() > TAILLE_MAXI) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_ERREUR_FORMAT);
-		}
-
-	}
-
-	private void validerEmail(String email, BusinessException businessException) {
-
-		if (email.isBlank() ^ email.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-		}
-
-		if (!email.contains("@")) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_ERREUR_FORMAT);
-		}
-	}
-
-	private void validerTelephone(String telephone, BusinessException businessException) {
-
-		if (telephone.isBlank() ^ telephone.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-		}
-
-		if (!telephone.matches(REGEX_TELEPHONE)) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_ERREUR_FORMAT);
-
-		}
-
-	}
-
-	private void validerRue(String rue, BusinessException businessException) {
-
-		if (rue.isBlank() ^ rue.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-		}
-	}
-
-	private void validercodePostal(String codePostal, BusinessException businessException) {
-
-		if (codePostal.isBlank() ^ codePostal.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-		}
-
-		if (!codePostal.matches(REGEX_CODE_POSTAL)) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_ERREUR_FORMAT);
-
-		}
-
-	}
-
-	private void validerVille(String ville, BusinessException businessException) {
-
-		if (ville.isBlank() ^ ville.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-
-		}
-		if (!ville.matches(REGEX_VILLE)) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_ERREUR_FORMAT);
-
-		}
-
-	}
-
-	private void validermotDePasse(String motdepasse, BusinessException businessException) {
-
-		if (motdepasse.isBlank() ^ motdepasse.isEmpty()) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_VIDE_OU_BLANC);
-		}
-		
-		if (!motdepasse.matches(REGEX_MOT_DE_PASSE)) {
-			businessException.ajouterErreur(CodesResultatBLL.REGLE_MDP);
-		}
-		
-		
-		
-	}
-
+	
 	public List<Utilisateur> selectionnerTousLesUtilisateurs() throws BusinessException, DALException {
 		return this.utilisateurDAO.selectAll();
 	}
@@ -180,4 +68,7 @@ public class UtilisateurManager {
 	public Utilisateur selectionnerUnUtilisateurAvecSonID(int id) throws BusinessException, DALException {
 		return this.utilisateurDAO.selectById(id);
 	}
-}
+	
+ }
+
+	
