5f90a44b60abdf0d9c79cc26fa97876887eb4985
alexgitENI <alexgalaxy67@gmail.com>
2022-05-30 11:55:25 +0200

    d

--- ProjetEncheres/src/main/java/fr/eni/encheres/ServletEncherir.java
+++ ProjetEncheres/src/main/java/fr/eni/encheres/ServletEncherir.java
@@ -33,6 +33,8 @@ public class ServletEncherir extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
+		
+		fd
 	}
 
 }
